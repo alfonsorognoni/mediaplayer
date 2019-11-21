a98782b6011ec046aa184be1151786181593d848
Alfonso Rognoni <alfonso@7lfreight.com>
2019-11-21 12:48:14 -0300

    ads position

--- assets/plugins/Ads/index.ts
+++ assets/plugins/Ads/index.ts
@@ -5,14 +5,18 @@ class AdsPlugin {
   private ads: Ads;
   private player: MediaPlayer;
   private media: HTMLMediaElement;
+  private currentAd: Ad;
+  private adsContainer: HTMLElement;
 
   constructor() {
     this.ads = Ads.getInstance();
+    this.adsContainer = document.createElement('div');
     this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
   }
 
   run(player: MediaPlayer) {
     this.player = player;
+    this.player.container.appendChild(this.adsContainer);
     this.media = this.player.media;
     this.media.addEventListener('timeupdate', this.handleTimeUpdate);
   }
@@ -25,8 +29,28 @@ class AdsPlugin {
   }
 
   private renderAd() {
+    if (this.currentAd) {
+      return;
+    }
+    console.log(this.currentAd);
+
     const ad = this.ads.getAd();
-    console.log(ad);
+    this.currentAd = ad;
+    this.adsContainer.innerHTML = `
+      <div class="ads">
+        <a class="ads__link" href="${this.currentAd.url}" target="_blank">
+          <img class="ads__img" src="${this.currentAd.imageUrl}" />
+          <div class="ads__info">
+            <h5 class="ads__title">${this.currentAd.title}</h5>
+            <p class="ads__body">${this.currentAd.body}</p>
+          </div>
+        </a>
+      </div>
+    `;
+    setTimeout(() => {
+      this.currentAd = null;
+      this.adsContainer.innerHTML = '';
+    }, 10000);
   }
 
 }
